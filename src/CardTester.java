4591494367be50f015697515394b4313b219885f
Scott Alan Pozder <pozdersa@s.dcsdk12.org>
2019-04-02 08:02:52 -0600

    creating card methods and testing them

--- src/CardTester.java
+++ src/CardTester.java
@@ -8,6 +8,21 @@ public class CardTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card ace = new Card("Ace", "Spades", 14);
+		Card two = new Card("Two", "Clubs", 2);
+		Card twoClubs = new Card("Two", "Clubs", 2);
+
+		System.out.println(ace.suit());
+		System.out.println(ace.rank());
+		System.out.println(ace.pointValue());
+
+		System.out.println("");
+
+		System.out.println(ace.matches(two));
+		System.out.println(two.matches(twoClubs));
+
+		System.out.println("");
+
+		System.out.println(ace.toString());
 	}
 }
